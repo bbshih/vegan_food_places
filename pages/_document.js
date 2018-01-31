8325b1fbcdcb0635f4335b485a03ff1913028658
billyshih <me@billyshih.com>
2018-01-30 17:42:50 -0800

    Add stylmed ocmponents

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,18 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
     const styleTags = sheet.getStyleElement()
     return { ...page, styleTags }
   }
 
-  render () {
+  render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <title>My page</title>
           {this.props.styleTags}
